bfa43925987ea7e981ab6da72b1951489ba27b3b
Nyura95 <taxisoopya@hotmail.fr>
2019-11-18 23:27:19 +0100

    update account

--- models/account.go
+++ models/account.go
@@ -15,6 +15,14 @@ type Account struct {
 	Firstname string `json:"first_name"`
 	Lastname  string `json:"last_name"`
 	Username  string `json:"username"`
+
+	Score   int    `json:"score"`
+	Level   int    `json:"level"`
+	Credit  int    `json:"credit"`
+	Phone   string `json:"phone"`
+	City    string `json:"city"`
+	Country string `json:"country"`
+
 	password  string
 	IDScope   int       `json:"idScope"`
 	CreatedAt time.Time `json:"createdAt"`
@@ -24,13 +32,13 @@ type Account struct {
 // FindAccount for find one account by id
 func FindAccount(id int) (*Account, error) {
 	account := Account{}
-	result, err := db.Query("SELECT id, id_avatars, first_name, last_name, username, password, id_scope, created_at, updated_at FROM tbl_account WHERE id = ?", id)
+	result, err := db.Query("SELECT id, id_avatars, first_name, last_name, username, score, level, credit, phone, city, country, password, id_scope, created_at, updated_at FROM tbl_account WHERE id = ?", id)
 	if err != nil {
 		return &account, err
 	}
 	defer result.Close()
 	for result.Next() {
-		err := result.Scan(&account.ID, &account.IDAvatars, &account.Firstname, &account.Lastname, &account.Username, &account.password, &account.IDScope, &account.CreatedAt, &account.UpdatedAt)
+		err := result.Scan(&account.ID, &account.IDAvatars, &account.Firstname, &account.Lastname, &account.Username, &account.Score, &account.Level, &account.Credit, &account.Phone, &account.City, &account.Country, &account.password, &account.IDScope, &account.CreatedAt, &account.UpdatedAt)
 		if err != nil {
 			panic(err.Error())
 		}
@@ -41,13 +49,13 @@ func FindAccount(id int) (*Account, error) {
 // FindAccountByPassword for find one account by password
 func FindAccountByPassword(password string) (*Account, error) {
 	account := Account{}
-	result, err := db.Query("SELECT id, id_avatars, first_name, last_name, username, password, id_scope, created_at, updated_at FROM tbl_account WHERE password = ?", password)
+	result, err := db.Query("SELECT id, id_avatars, first_name, last_name, username, score, level, credit, phone, city, country, password, id_scope, created_at, updated_at FROM tbl_account WHERE password = ?", password)
 	if err != nil {
 		return &account, err
 	}
 	defer result.Close()
 	for result.Next() {
-		err := result.Scan(&account.ID, &account.IDAvatars, &account.Firstname, &account.Lastname, &account.Username, &account.password, &account.IDScope, &account.CreatedAt, &account.UpdatedAt)
+		err := result.Scan(&account.ID, &account.IDAvatars, &account.Firstname, &account.Lastname, &account.Username, &account.Score, &account.Level, &account.Credit, &account.Phone, &account.City, &account.Country, &account.password, &account.IDScope, &account.CreatedAt, &account.UpdatedAt)
 		if err != nil {
 			panic(err.Error())
 		}
@@ -58,14 +66,14 @@ func FindAccountByPassword(password string) (*Account, error) {
 // FindAllAccount for find all accounts in the database
 func FindAllAccount() ([]*Account, error) {
 	accounts := []*Account{}
-	result, err := db.Query("SELECT id, id_avatars, first_name, last_name, username, password, id_scope, created_at, updated_at FROM tbl_account")
+	result, err := db.Query("SELECT id, id_avatars, first_name, last_name, username, score, level, credit, phone, city, country, password, id_scope, created_at, updated_at FROM tbl_account")
 	if err != nil {
 		return accounts, err
 	}
 	defer result.Close()
 	for result.Next() {
 		account := Account{}
-		err := result.Scan(&account.ID, &account.IDAvatars, &account.Firstname, &account.Lastname, &account.Username, &account.password, &account.IDScope, &account.CreatedAt, &account.UpdatedAt)
+		err := result.Scan(&account.ID, &account.IDAvatars, &account.Firstname, &account.Lastname, &account.Username, &account.Score, &account.Level, &account.Credit, &account.Phone, &account.City, &account.Country, &account.password, &account.IDScope, &account.CreatedAt, &account.UpdatedAt)
 		if err != nil {
 			panic(err.Error())
 		}
@@ -99,13 +107,13 @@ func CheckAccountExist(IDAccounts ...int) bool {
 
 // Update a user
 func (a *Account) Update() error {
-	stmt, err := db.Prepare("UPDATE tbl_account SET avatars = ?, firstname = ?, lastname = ?, username = ?, password = ?, id_scope = ?, updated_at = ? WHERE id = ?")
+	stmt, err := db.Prepare("UPDATE tbl_account SET avatars = ?, firstname = ?, lastname = ?, username = ?, score = ?, level = ?, credit = ?, phone = ?, city = ?, country = ?, password = ?, id_scope = ?, updated_at = ? WHERE id = ?")
 	if err != nil {
 		return err
 	}
 	defer stmt.Close()
 
-	_, err = stmt.Exec(a.IDAvatars, a.Firstname, a.Lastname, a.Username, a.password, a.IDScope, time.UTC, a.ID)
+	_, err = stmt.Exec(a.IDAvatars, a.Firstname, a.Lastname, a.Username, a.Score, a.Level, a.Credit, a.Phone, a.City, a.Country, a.password, a.IDScope, time.UTC, a.ID)
 	if err != nil {
 		return err
 	}
@@ -114,9 +122,9 @@ func (a *Account) Update() error {
 }
 
 // CreateAccount create a new user
-func CreateAccount(IDAvatars int, firstname string, lastname string, username string, password string, idScope int) (*Account, error) {
+func CreateAccount(IDAvatars int, firstname string, lastname string, username string, score int, level int, credit int, phone string, city string, country string, password string, idScope int) (*Account, error) {
 	account := &Account{}
-	stmt, err := db.Prepare("INSERT INTO tbl_account(id_avatars, first_name, last_name, username, password, id_scope, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)")
+	stmt, err := db.Prepare("INSERT INTO tbl_account(id_avatars, first_name, last_name, username, score, level, credit, phone, city, country, password, id_scope, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
 	if err != nil {
 		return account, err
 	}
@@ -127,7 +135,7 @@ func CreateAccount(IDAvatars int, firstname string, lastname string, username st
 	hasher := md5.New()
 	hasher.Write([]byte(username + ":" + password))
 
-	result, err := stmt.Exec(IDAvatars, firstname, lastname, username, hex.EncodeToString(hasher.Sum(nil)), idScope, utc, utc)
+	result, err := stmt.Exec(IDAvatars, firstname, lastname, username, score, level, credit, phone, city, country, hex.EncodeToString(hasher.Sum(nil)), idScope, utc, utc)
 	if err != nil {
 		return account, err
 	}
--- server/v1/account.go
+++ server/v1/account.go
@@ -7,5 +7,8 @@ import (
 
 // CreateAccount for create a new account
 func CreateAccount(w http.ResponseWriter, r *http.Request) {
+	var body login
+	decoder := json.NewDecoder(r.Body)
+	decoder.Decode(&body)
 	json.NewEncoder(w).Encode("ok")
 }
