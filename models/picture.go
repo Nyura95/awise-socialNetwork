987feee2a490364122c651c8982fd59e14959e02
Alexis Courapied <acourapied@enovee.eu>
2019-11-20 15:49:26 +0100

    up

--- models/picture.go
+++ models/picture.go
@@ -5,26 +5,28 @@ import (
 	"time"
 )
 
-// Picture table model
+// Picture table models
 type Picture struct {
 	ID        int       `json:"id"`
 	Path      string    `json:"path"`
 	Origin    string    `json:"origin"`
 	Size      string    `json:"size"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	Active    int       `json:"active"`
+	Delete    int       `json:"delete"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
 }
 
 // FindPicture for find one picture by id
 func FindPicture(id int) (*Picture, error) {
 	picture := Picture{}
-	result, err := db.Query("SELECT id, path, origin, size, created_at, updated_at FROM tbl_pictures WHERE id = ? LIMIT 1", id)
+	result, err := db.Query("SELECT id, path, origin, size, active, `delete`, created_at, updated_at FROM tbl_pictures WHERE id = ?", id)
 	if err != nil {
 		return &picture, err
 	}
 	defer result.Close()
 	for result.Next() {
-		err := result.Scan(&picture.ID, &picture.Path, &picture.Origin, &picture.Size, &picture.CreatedAt, &picture.UpdatedAt)
+		err := result.Scan(&picture.ID, &picture.Path, &picture.Origin, &picture.Size, &picture.Active, &picture.CreatedAt, &picture.UpdatedAt)
 		if err != nil {
 			panic(err.Error())
 		}
@@ -32,34 +34,34 @@ func FindPicture(id int) (*Picture, error) {
 	return &picture, nil
 }
 
-// FindAllPicture for find all access_token in the database
-func FindAllPicture() ([]*Picture, error) {
-	pictures := []*Picture{}
-	result, err := db.Query("SELECT id, path, origin, size, created_at, updated_at FROM tbl_pictures")
+// FindAllPictures for find all avatars in the database
+func FindAllPictures() ([]*Picture, error) {
+	avatars := []*Picture{}
+	result, err := db.Query("SELECT id, path, origin, size, active, `delete`, created_at, updated_at FROM tbl_pictures")
 	if err != nil {
-		return pictures, err
+		return avatars, err
 	}
 	defer result.Close()
 	for result.Next() {
 		picture := Picture{}
-		err := result.Scan(&picture.ID, &picture.Path, &picture.Origin, &picture.Size, &picture.CreatedAt, &picture.UpdatedAt)
+		err := result.Scan(&picture.ID, &picture.Path, &picture.Origin, &picture.Size, &picture.Active, &picture.CreatedAt, &picture.UpdatedAt)
 		if err != nil {
 			panic(err.Error())
 		}
-		pictures = append(pictures, &picture)
+		avatars = append(avatars, &picture)
 	}
-	return pictures, nil
+	return avatars, nil
 }
 
-// Update a picture
-func (p *Picture) Update() error {
-	stmt, err := db.Prepare("UPDATE tbl_pictures SET path = ?, origin = ?, size = ?, updated_at = ? WHERE id = ?")
+// Update a user
+func (a *Picture) Update() error {
+	stmt, err := db.Prepare("UPDATE tbl_pictures SET path = ?, origin = ?, size = ?, active = ?, `delete` = ?, updated_at = ? WHERE id = ?")
 	if err != nil {
 		return err
 	}
 	defer stmt.Close()
 
-	_, err = stmt.Exec(p.Path, p.Origin, p.Size, helpers.GetUtc(), p.ID)
+	_, err = stmt.Exec(a.Path, a.Origin, a.Size, a.Active, a.Delete, time.UTC, a.ID)
 	if err != nil {
 		return err
 	}
@@ -67,26 +69,28 @@ func (p *Picture) Update() error {
 	return nil
 }
 
-// NewPicture create a new access_token
+// NewPicture create a new picture
 func NewPicture(path string, origin string, size string) (*Picture, error) {
-	stmt, err := db.Prepare("INSERT INTO tbl_pictures(path, origin, size, created_at, updated_at) VALUES (?, ?, ?, ?, ?)")
+	picture := &Picture{}
+	stmt, err := db.Prepare("INSERT INTO tbl_account(path, origin, size, active, `delete`, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?)")
 	if err != nil {
-		return nil, err
+		return picture, err
 	}
 	defer stmt.Close()
+
 	utc := helpers.GetUtc()
 
-	result, err := stmt.Exec(path, origin, size, utc, utc)
+	result, err := stmt.Exec(path, origin, size, 0, 0, utc, utc)
 	if err != nil {
-		return nil, err
+		return picture, err
 	}
 
 	ID, err := result.LastInsertId()
 	if err != nil {
-		return nil, err
+		return picture, err
 	}
 
-	picture, _ := FindPicture(int(ID))
+	picture, _ = FindPicture(int(ID))
 
 	return picture, nil
 }
